3dab81ff867af030b3c631c04e33c634c7485e64
Alexod24 <alex.od2408@gmail.com>
2024-05-23 00:55:10 -0500

    Avance Jueves

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
 const hambIcon = document.querySelector(".hamb");
+const navMobile = document.querySelector(".nav");
 
 hambIcon.addEventListener("click", function (){
-    hambIcon.classList.toggle("active")
+    hambIcon.classList.toggle("active");
+    navMobile.classList.toggle("slide");
 })
+
+console.log("Hola mundo")
+
+//Animaciones del header 
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", scrollY > 0);
+});
