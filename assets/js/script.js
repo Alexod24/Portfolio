bfe0c168339588721df7e7c12cf1ab617e436f63
Alexod24 <alex.od2408@gmail.com>
2024-05-23 23:34:47 -0500

    New Portfolio final

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,15 @@ const navMobile = document.querySelector(".nav");
 hambIcon.addEventListener("click", function (){
     hambIcon.classList.toggle("active");
     navMobile.classList.toggle("slide");
-})
+});
+
+
+document.onclick = function (n) {
+    if(n.target.id !== "hamb" && n.target.it !== "nav__menu"){
+        hambIcon.classList.remove("active");
+        navMobile.classList.remove("slide");
+    }
+};
 
 console.log("Hola mundo")
 
@@ -15,3 +23,13 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", scrollY > 0);
 });
+
+const containerEl =document.querySelector(".portfolio__gallery");
+
+var mixer = mixitup(containerEl,{
+    animation: {
+        enable: false,
+    },
+});
+
+mixer.filter("*");
